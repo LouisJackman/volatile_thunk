27503879547bdb02c5cfa88712346415735ed26d
LouisJackman <LouisJackman@users.noreply.github.com>
2018-11-14 21:23:11 +0000

    Add script for composing a new post

---
+++ compose.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+from datetime import datetime
+from os import environ
+from textwrap import dedent
+from typing import Set
+from pathlib import Path
+
+
+def title_to_path_component(title: str) -> str:
+    without_spaces = re.sub(r'\s+', '-', title.lower())
+    return re.sub(r'[^\w-]', '', without_spaces)
+
+
+def make_path(title: str, when: datetime) -> Path:
+    day, month, year = when.day, when.month, when.year
+    title_component = title_to_path_component(title)
+
+    return (
+        Path('content')
+        / 'posts'
+        / f'{year:04}'
+        / f'{month:02}'
+        / f'{day:02}'
+        / title_component
+        / 'post.md'
+    )
+
+
+def make_post_file(title: str, tags: Set[str]) -> Path:
+    now = datetime.now()
+    year, month, day = now.year, now.month, now.day
+    hour, minute = now.hour, now.minute
+
+    path = make_path(title, now)
+    tags = ', '.join(tags)
+
+    header = dedent(f'''
+        Title: {title}
+        Date: {year:04}-{month:02}-{day:02} {hour:02}:{minute:02}
+        Tags: {tags}
+
+
+    ''').lstrip()
+
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open('w') as post:
+        post.write(header)
+
+    return path
+
+
+def get_editor() -> str:
+    if 'VISUAL' in environ:
+        result = environ['VISUAL']
+    elif 'EDITOR' in environ:
+        result = environ['EDITOR']
+    else:
+        result = 'vi'
+    return result
+
+
+def edit(path: Path) -> subprocess.CompletedProcess:
+    editor = get_editor()
+    return subprocess.check_call([editor, str(path)])
+
+
+def main():
+    title = input("The post's title: ")
+    raw_tags = input("The post's tags (seperate with commas): ")
+    tags = {tag.lower().strip() for tag in raw_tags.split(',')}
+
+    post_file = make_post_file(title, tags)
+    edit(post_file)
+
+
+if __name__ == '__main__':
+    main()
+
